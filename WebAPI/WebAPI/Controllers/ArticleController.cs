529310cbd4cc10c610f0be4096620ea182902f07
Seb-Dz <sebastienduruz@protonmail.com>
2025-03-06 21:18:39 +0100

    Basic Web API with controllers

--- WebAPI/WebAPI/Controllers/ArticleController.cs
+++ WebAPI/WebAPI/Controllers/ArticleController.cs
@@ -15,15 +15,54 @@ namespace WebAPI.Controllers
         }
 
         [HttpGet(), Route("/")]
-        public IEnumerable<Article> Get()
+        public async Task<ActionResult<IEnumerable<Article>>> Get()
         {
-            return _dbContext.Articles;
+            return Ok(_dbContext.Articles);
         }
 
         [HttpGet(), Route("/{id}")]
-        public Article Get(int id)
+        public async Task<ActionResult<Article>> Get(int id)
         {
-            return _dbContext.Articles.FirstOrDefault(x => x.Id == id);
+            if(_dbContext.Articles.Any(x => x.Id == id))
+                return Ok(_dbContext.Articles.First(x => x.Id == id));
+            return NotFound();
+        }
+
+        [HttpPost(), Route("/")]
+        public async Task<ActionResult<Article>> Post([FromBody]Article article)
+        {
+            article.Id = _dbContext.Articles.Count + 1;
+            _dbContext.Articles.Add(article);
+
+            return Created();
+        }
+
+        [HttpDelete(), Route("/{id}")]
+        public async Task<ActionResult<Article>> Delete(int id)
+        {
+            if(_dbContext.Articles.Any(x => x.Id == id))
+            {
+                _dbContext.Articles.Remove(_dbContext.Articles.First(x => x.Id == id));
+                return NoContent();
+            }
+
+            return NotFound();
+        }
+
+        [HttpPut(), Route("/{id}")]
+        public async Task<ActionResult<Article>> Update(int id, [FromBody]Article article)
+        {
+            if(_dbContext.Articles.Any(x => x.Id == id))
+            {
+                Article articleToUpdate = _dbContext.Articles.First(x => x.Id == id);
+                articleToUpdate.Title = article.Title;
+                articleToUpdate.Content = article.Content;
+                articleToUpdate.Date = DateTime.Now;
+
+                return NoContent();
+            }
+
+            return NotFound();
         }
     }
 }
